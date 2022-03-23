21437be362a88a222fae828e7497239278fe94d6
larissa <laramina984@gmail.com>
2022-03-23 11:07:38 -0300

    Exemplo switch.

---
+++ aula5/ex3.php
@@ -0,0 +1,10 @@
+<?php
+    $op = 'a';
+    switch($op){
+      case 'a': echo 'Abrir'; break;
+      case 'f': echo 'Fechar'; break;
+      case 's': echo 'Salvar'; break;
+      case 's': echo 'Esperar'; break;
+      default: echo 'Sair'; break;
+    }
+?>
