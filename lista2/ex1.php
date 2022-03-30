a6f16763198489ddd61ea8c7e6c91db525167034
larissa <laramina984@gmail.com>
2022-03-30 11:25:20 -0300

    ex1L2.php

---
+++ lista2/ex1.php
@@ -0,0 +1,13 @@
+<?php
+    $soma = 0;
+    for($n=0;$n<=100;$n++){
+        if($n%2 ==0){
+            $soma = $soma + $n;  
+        }      
+    }
+    echo "A soma dos numeros pares é ".$soma;
+?>
+
+
+
+
